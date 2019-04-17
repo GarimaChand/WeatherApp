eab8d2b96460bf8a7036317407fdfc36ee8dac65
garimachand <garimachand011@gmail.com>
2019-04-17 10:03:14 -0700

    weatherapp

---
+++ app/src/main/java/co/example/weatherapp/DownloadTask.java
@@ -0,0 +1,70 @@
+package com.example.mainactivity.;
+
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+public class DownloadTask extends AsyncTask<String,Void,String> {
+
+    @Override
+    protected String doInBackground(String... urls) {
+        String result = "";
+        URL url;
+        HttpURLConnection urlConnection = null;
+
+
+        try {
+            url = new URL(urls[0]);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = urlConnection.getInputStream();
+
+            InputStreamReader reader = new InputStreamReader(in);
+
+            int data = reader.read();
+
+            while ( data != -1){
+                char current = (char) data;
+                result += current;
+                data = reader.read();
+            }
+
+            return  result;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        try {
+            JSONObject jsonObject = new JSONObject(result);
+            JSONObject weatherData = new JSONObject(jsonObject.getString("main"));
+            Double temperature = Double.parseDouble(weatherData.getString("temp"));
+            // formula for F
+            int temperatureInteger = (int) (temperature * 1.8 - 459.67);
+            // to give user the name and place where they are
+            String placeName = jsonObject.getString("name");
+
+            com.example.mainactivity.MainActivity.temperatureTextView.setText(String.valueOf(temperatureInteger));
+            com.example.mainactivity.MainActivity.placeTextView.setText(placeName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ app/src/main/java/co/example/weatherapp/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.mainactivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.LocaleList;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.mainactivity.R;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    static TextView placeTextView;
+    static TextView temperatureTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        placeTextView = (TextView) findViewById(R.id.nameTextView);
+        temperatureTextView = (TextView) findViewById(R.id.temperatureTextView);
+
+
+        // this gives the location of the user
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        String provider = locationManager.getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        //get latitude
+        Double lat = location.getLatitude();
+        //get longitude
+        Double lng = location.getLongitude();
+
+        com.example.mainactivity.DownloadTask task = new com.example.mainactivity.DownloadTask();
+        // give latitude and longitude
+        task.execute("https://samples.openweathermap.org/data/2.5/weather?lat=" + String.valueOf(lat) + "&lon=" + String.valueOf(lng)+" &appid=c9f3c2baddf39b53b0a40d262866dfcc");
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
